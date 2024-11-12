c605cdfd3b5db77bd175bba5451217c7943328d5
Mykyta Astashov <nikastpro228@gmail.com>
2024-11-12 19:36:07 +0200

    added new file

---
+++ ЛР 3.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include "stdio.h"
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	double a=1, b=2, c=3, h=4, S, P;
+	S = (a + b) * 0.5 * h;
+	P = a * b * 2 * c;
+	cout << "S =" << S << endl;
+	cout << "P =" << P << endl;
+	system("pause");
+	return 0;
+
+
+} 
+
+
+
