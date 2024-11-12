af2c3522ffbd336b5c4a6f5a29be25b508e4e6e0
Mykyta Astashov <nikastpro228@gmail.com>
2024-11-12 23:27:00 +0200

    added new file

---
+++ LR_8.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include "stdio.h"
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Новий файл для гілки LR8_br";
+
+
+
+} 
+
+
+
