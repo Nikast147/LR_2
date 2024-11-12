55c0bab3bdf95917918fbaf316da11965f9778e0
Mykyta Astashov <nikastpro228@gmail.com>
2024-11-12 20:02:20 +0200

    LR_3 file changed

--- LR_3.cpp
+++ LR_3.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	double a=1, b=2, c=3, h=4, S, P;
 	S = (a + b) * 0.5 * h;
 	P = a * b * 2 * c;
 	cout << "S =" << S << endl;
 	cout << "P =" << P << endl;
+	cout << "Можливість писати кирилицею";
 	system("pause");
 	return 0;
 
